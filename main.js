5ad94b153cd8b11aa47aba34fc98d495a1657e36
Sysliklol <vkharko@gmail.com>
2016-10-09 21:22:14 +0300

    final

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 $(function(){
       var LIST = $('.bl-list');
       var TEMP = $(".bl-row").html();
-      var TEMPRIGHT= $("#add").html();
-      var LISTRIGHT =$(".bought-container");
+      var TEMPRIGHT= $(".bought-container").html();
+      var LISTRIGHT =$(".add");
+      var TEMPBOUGHT= $(".add-bought-container").html();
+      var LISTBOUGHT =$(".addBought");
       addItem("Помідори");
       $("#addButton").click(function (){
             var log = $(".add_inputer");
@@ -11,10 +13,14 @@ $(function(){
       });
       function addItem(name) {
           var n = 1;
+          var check = 1;
           var val = name;
           if (val) {
               var node = $(TEMP);
+              node.show();
               var nodeRight = $(TEMPRIGHT);
+              nodeRight.show();
+              var nodeBought = $(TEMPBOUGHT);
               $(node).find('.b-product').text(val);
               $(node).find('.bl-label').text(n);
               $(nodeRight).find('.item-bought').text(val);
@@ -37,6 +43,34 @@ $(function(){
                       n--;
                   }
               });
+              $(node).find('.bl-buy').click(function(){
+                  console.log(check);
+                  if(check==1) {
+                      nodeBought.show();
+                      $(node).find('.bl-x').hide();
+                      $(node).find('.bl-plus').hide();
+                      $(node).find('.bl-minus').hide();
+                      $(node).addClass('.state-bought');
+                      $(node).find('.bl-buy').text("Не куплене");
+                      $(nodeBought).find('.item-bought').text(val);
+                      $(nodeBought).find('.item-bought-count').text(n);
+
+                      LISTBOUGHT.append(nodeBought);
+                      nodeRight.remove();
+                      check = 0;
+
+                  }
+                  else{
+                      $(node).find('.bl-x').show();
+                      $(node).find('.bl-plus').show();
+                      $(node).find('.bl-minus').show();
+                      nodeBought.hide();
+                      $(node).find('.bl-buy').text("Куплене");
+                      check = 1;
+                      LISTRIGHT.append(nodeRight);
+                      nodeBought.remove();
+                  }
+              })
           }
       }
 });
